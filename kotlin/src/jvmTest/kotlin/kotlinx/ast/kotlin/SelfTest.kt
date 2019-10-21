6ea4fbc1551553677567bc8459987db8ee3dcf84
Dennis Rieks <dennisrieks@googlemail.com>
2019-10-21 23:34:18 +0200

    exclude some files from self-test refs #3

--- kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
+++ kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
@@ -1,3 +1,10 @@
 package kotlinx.ast.kotlin
 
-class SelfTest : DirectoryTest(pathOf(".").pathMap())
+class SelfTest : DirectoryTest(pathOf(".").pathMap().filterNot { (file, _) ->
+    // skip this files: currently too slow..
+    setOf(
+        "KotlinLexer.kt",
+        "KotlinParser.kt",
+        "UnicodeClasses.kt"
+    ).contains(file.name)
+})
