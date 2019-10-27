fd6123da028e211f003b0a0725bbfc8db99655a4
Dennis Rieks <dennisrieks@googlemail.com>
2019-10-27 23:02:55 +0100

    skip slow tests in jitpack

--- kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
+++ kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
@@ -1,3 +1,14 @@
 package kotlinx.ast.kotlin
 
-class SelfTest : DirectoryTest(pathOf(".").pathMap())
+class SelfTest : DirectoryTest(pathOf(".").pathMap().filterNot { (file, _) ->
+    if (System.getenv("JITPACK") == "true") {
+        // skip this files in jitpack: currently too slow..
+        setOf(
+            "KotlinLexer.kt",
+            "KotlinParser.kt",
+            "UnicodeClasses.kt"
+        ).contains(file.name)
+    } else {
+        false
+    }
+})
