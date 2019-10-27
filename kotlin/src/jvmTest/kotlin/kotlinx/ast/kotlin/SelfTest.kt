ef9feac1cdf86d20a5e9c29f6d45d82a27e693af
Dennis Rieks <dennisrieks@googlemail.com>
2019-10-27 22:19:38 +0100

    include all kotlin files in SelfTest.kt refs #3

--- kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
+++ kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/SelfTest.kt
@@ -1,10 +1,3 @@
 package kotlinx.ast.kotlin
 
-class SelfTest : DirectoryTest(pathOf(".").pathMap().filterNot { (file, _) ->
-    // skip this files: currently too slow..
-    setOf(
-        "KotlinLexer.kt",
-        "KotlinParser.kt",
-        "UnicodeClasses.kt"
-    ).contains(file.name)
-})
+class SelfTest : DirectoryTest(pathOf(".").pathMap())
