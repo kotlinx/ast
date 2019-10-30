13212b2c6f8fd1439040380a953d894286de680a
Dennis Rieks <dennisrieks@googlemail.com>
2019-10-30 23:12:51 +0100

    moved examples

--- grammar-kotlin-parser-antlr-java/src/test/kotlin/kotlinx/ast/example/ExampleMain.kt
+++ grammar-kotlin-parser-antlr-java/src/test/kotlin/kotlinx/ast/example/ExampleMain.kt
@@ -1,12 +1,15 @@
-package kotlinx.ast.grammar.kotlin.target.antlr.java
+package kotlinx.ast.example
 
 import kotlinx.ast.common.AstSource
 import kotlinx.ast.common.ast.Ast
 import kotlinx.ast.common.print
 import kotlinx.ast.grammar.kotlin.common.summary
+import kotlinx.ast.grammar.kotlin.target.antlr.java.KotlinGrammarAntlrJavaParser
 
 fun main() {
-    val source = AstSource.File("kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/ExampleMain.kt")
+    val source = AstSource.File(
+        "grammar-kotlin-parser-antlr-java/src/test/kotlin/kotlinx/ast/example/ExampleMain.kt"
+    )
     val kotlinFile = KotlinGrammarAntlrJavaParser.parseKotlinFile(source)
     kotlinFile.summary()
         .onSuccess { astList ->
--- grammar-kotlin-parser-antlr-kotlin/src/jvmTest/kotlin/kotlinx/ast/example/ExampleMain.kt
+++ grammar-kotlin-parser-antlr-kotlin/src/jvmTest/kotlin/kotlinx/ast/example/ExampleMain.kt
@@ -1,12 +1,15 @@
-package kotlinx.ast.grammar.kotlin.target.antlr.kotlin
+package kotlinx.ast.example
 
 import kotlinx.ast.common.AstSource
 import kotlinx.ast.common.ast.Ast
 import kotlinx.ast.common.print
 import kotlinx.ast.grammar.kotlin.common.summary
+import kotlinx.ast.grammar.kotlin.target.antlr.kotlin.KotlinGrammarAntlrKotlinParser
 
 fun main() {
-    val source = AstSource.File("kotlin/src/jvmTest/kotlin/kotlinx/ast/kotlin/ExampleMain.kt")
+    val source = AstSource.File(
+        "grammar-kotlin-parser-antlr-kotlin/src/jvmTest/kotlin/kotlinx/ast/example/ExampleMain.kt"
+    )
     val kotlinFile = KotlinGrammarAntlrKotlinParser.parseKotlinFile(source)
     kotlinFile.summary()
         .onSuccess { astList ->
