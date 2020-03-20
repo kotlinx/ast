f9e3f6d260fefeb614b908263f012fe851dbe7d5
Dennis Rieks <dennisrieks@googlemail.com>
2020-03-20 23:26:02 +0100

    workaround for issue #10

--- parser-antlr-kotlin/src/jvmMain/kotlin/kotlinx/ast/parser/antlr/kotlin/AntlrKotlinCharStream.kt
+++ parser-antlr-kotlin/src/jvmMain/kotlin/kotlinx/ast/parser/antlr/kotlin/AntlrKotlinCharStream.kt
@@ -3,11 +3,24 @@ package kotlinx.ast.parser.antlr.kotlin
 import kotlinx.ast.common.AstSource
 import org.antlr.v4.kotlinruntime.CharStream
 import org.antlr.v4.kotlinruntime.CharStreams
+import java.nio.file.FileSystems
+import java.nio.file.Files
 
 internal actual fun AstSource.toAntlrKotlinCharStream(): CharStream {
     return when (this) {
-        is AstSource.File ->
+        is AstSource.File -> {
+            /*
+            // There is a Bug in CharStreams.fromFileName:
+            // https://github.com/Strumenta/antlr-kotlin/issues/38
+            // https://github.com/kotlinx/ast/issues/10
             CharStreams.fromFileName(filename)
+            // Workaround:
+            // first reading the file into a string
+            */
+            val path = FileSystems.getDefault().getPath(filename)
+            val content = Files.readString(path)
+            CharStreams.fromString(content)
+        }
         is AstSource.String ->
             CharStreams.fromString(content)
     }
