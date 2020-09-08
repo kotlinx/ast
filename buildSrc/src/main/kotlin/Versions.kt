d7838967faa5d346a546cc4de69ded13bea06926
Dennis Rieks <dennisrieks@googlemail.com>
2020-09-08 22:31:26 +0200

    versions const quickfix

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,16 +1,16 @@
 object Versions {
-    val kotlin = "1.3.70"
-    val gradle = "5.6.2"
-    val antlrJava = "4.7.2"
-    val antlrOptimized = "4.7.4"
-    val antlrUsedByAntlrKotlin = "4.7.1"
+    const val kotlin = "1.3.70"
+    const val gradle = "5.6.2"
+    const val antlrJava = "4.7.2"
+    const val antlrOptimized = "4.7.4"
+    const val antlrUsedByAntlrKotlin = "4.7.1"
     // https://github.com/drieks/antlr-kotlin/commit/b09d76328f7d8c38417174d5621bbdfb52b6405e
     // https://jitpack.io/#drieks/antlr-kotlin
-    val antlrKotlin = "b09d76328f"
+    const val antlrKotlin = "b09d76328f"
     /*
     // official antlr kotlin version
     val antlrKotlinGroup = "com.strumenta.antlr-kotlin"
     */
     // antlr kotlin version patched by kotlinx.ast author
-    val antlrKotlinGroup = "com.github.drieks.antlr-kotlin"
+    const val antlrKotlinGroup = "com.github.drieks.antlr-kotlin"
 }
