b5a286452cf113467a703d435c0a92dd73e00c2a
Dennis Rieks <dennisrieks@googlemail.com>
2020-10-22 00:05:24 +0200

    updated antlrKotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val antlrUsedByAntlrKotlin = "4.7.1"
     // https://github.com/drieks/antlr-kotlin/commit/b09d76328f7d8c38417174d5621bbdfb52b6405e
     // https://jitpack.io/#drieks/antlr-kotlin
-    const val antlrKotlin = "b09d76328f"
+    const val antlrKotlin = "ce5a7e161a"
     /*
     // official antlr kotlin version
     val antlrKotlinGroup = "com.strumenta.antlr-kotlin"
