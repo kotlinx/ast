ad21ce6141452799de7d37d1800b4a08450bf025
Dennis Rieks <dennisrieks@googlemail.com>
2021-07-25 23:59:47 +0200

    updated antlr kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
 
     // https://github.com/drieks/antlr-kotlin/commit/b09d76328f7d8c38417174d5621bbdfb52b6405e
     // https://jitpack.io/#drieks/antlr-kotlin
-    const val antlrKotlin = "121fb9ef5a"
+    const val antlrKotlin = "710f0f8b7f"
 
     /*
     // official antlr kotlin version
