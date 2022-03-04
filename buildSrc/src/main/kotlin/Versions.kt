713b5147693f73c208caa8366ce9f1a9a5b7a3ee
Dennis Rieks <dennisrieks@googlemail.com>
2022-03-04 23:32:14 +0100

    updated to kotest 5.1.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -16,5 +16,5 @@ object Versions {
     // antlr kotlin version patched by kotlinx.ast author
     const val antlrKotlinGroup = "com.github.drieks.antlr-kotlin"
 
-    const val kotest = "5.0.3"
+    const val kotest = "5.1.0"
 }
